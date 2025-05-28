be157af92ed928d52f61c9f2aa2d74124a097eca
N-I-V-Z <vothanhnhan106@gmail.com>
2025-05-28 22:47:35 +0700

    add ai chat

--- src/component/common/theme/masterLayout/index.js
+++ src/component/common/theme/masterLayout/index.js
@@ -1,12 +1,14 @@
 import { memo } from "react";
 import Header from "../header";
 import Footer from "../footer";
+import ChatWidget from "../../../chatAI/ChatWidget";
 
 const masterLayout = ({ children, ...props }) => {
   return (
     <div {...props}>
       <Header />
       {children}
+      <ChatWidget />;
       <Footer />
     </div>
   );
